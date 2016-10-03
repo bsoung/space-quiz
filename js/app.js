83db5d960406209bde00de5d1964208856b5e192
tpecorino <tpecorino@gmail.com>
2016-10-03 10:25:10 -0500

    Added state object and more base files.

---
+++ js/app.js
@@ -0,0 +1,17 @@
+var currentIdx = Math.floor(Math.random() * state.quiz.length);
+var currentQuestion = state.quiz.(currentIdx);
+
+
+// start quiz
+  // button.submit
+
+
+// restart quiz
+  // button.click
+
+
+// submit answer
+  //button.click
+
+
+// renderHtml 
