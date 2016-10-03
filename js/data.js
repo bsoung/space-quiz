66d83a0a703af086407442aa8d3b60edf0ae46fc
tpecorino <tpecorino@gmail.com>
2016-10-03 10:26:09 -0500

    Added state object and more base files.

---
+++ js/data.js
@@ -0,0 +1,36 @@
+var state = {
+  quiz: [
+    {
+      question: 'Which planet is NOT a gas giant?',
+      choice: ['Saturn', 'Neptune', 'Venus', 'Jupiter'],
+      answer: 'Venus'
+    },
+    {
+      question: 'What natural satalite orbits Earth?',
+      choice: ['Asteroid', 'The moon', 'Another planet', 'The international space station'],
+      answer: 'The moon'
+    },
+    {
+      question: 'The planet closest to the sun is?',
+      choice: ['Mercury', 'Venus', 'Mars', 'Earth'],
+      answer: 'Mercury'
+    },
+    {
+      question: 'What is the name of the galaxy we live in?',
+      choice: ['Andromeda', 'Milky Way', 'Bode\'s', 'Mayall'],
+      answer: 'Milky Way'
+    },
+    {
+      question: 'What planet is known for having a ring around it?',
+      choice: ['Mars', 'Jupiter', 'Venus', 'Saturn'],
+      answer: 'Saturn'
+    },
+    {
+      question: 'Which two planets have a retrograde(backwards) rotation?',
+      choice: ['Earth and Mars', 'Mars and Venus', 'Jupiter and Saturn', 'Venus and Uranus'],
+      answer: 'Venus and Uranus'
+    }
+  ],
+  correctFlag: false,
+  numberCorrect: 0   
+}
