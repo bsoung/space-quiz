6f1c46490cab21acd4eb5e762c2c1ca1b721cd24
tpecorino <tpecorino@gmail.com>
2016-10-03 12:21:31 -0500

    Built html rendering function.

--- js/app.js
+++ js/app.js
@@ -1,20 +1,34 @@
-// var currentIdx = Math.floor(Math.random() * state.quiz.length);
-// var currentQuestion = state.quiz(currentIdx);
+var questionContainer = $('.question-container');
+var questionCount = 1;
 
+function renderHtml (state, idx, element) {
+  var questionHtml = '<div class="question-container"><h2 class="question">';
+  questionHtml += state.quiz[idx].question + '<span>(';
+  questionHtml += questionCount + ' of ' + state.quiz.length;
+  questionHtml += ')</span></h2><form>';
 
-
-// start quiz
-function startQuiz() {
-  $('.start').click(function() {
-    console.log('hi')
-    $('.intro').hide();
-    $('.start').hide();
+  state.quiz[idx].choices.forEach(function (choice) {
+    questionHtml += '<div class="choices"><input type="radio" name="choice"<label>' 
+    questionHtml += choice + '</label>';
   });
-  
 }
 
-startQuiz();
-  // button.submit
+// Start Quiz 
+function startQuiz() {
+  $('.start').click(function() {
+    $('.intro, .start').hide();    
+    questionContainer.show();
+    state.currentIdx = state.quiz.length-1;
+  })  
+}
+
+
+// $('form input').on('change', function (){
+//   console.log($('input:radio:checked').next().next('label:first').html());
+// });
+
+
+// button.submit
 
 
 // restart quiz
@@ -25,4 +39,13 @@ startQuiz();
   //button.click
 
 
-// renderHtml 
+
+
+function main() {
+  questionContainer.hide();
+  startQuiz();
+}
+
+$(document).ready(main());
+
+
--- js/data.js
+++ js/data.js
@@ -2,35 +2,36 @@ var state = {
   quiz: [
     {
       question: 'Which planet is NOT a gas giant?',
-      choice: ['Saturn', 'Neptune', 'Venus', 'Jupiter'],
+      choices: ['A. Saturn', 'B. Neptune', 'C. Venus', 'D. Jupiter'],
       answer: 'Venus'
     },
     {
       question: 'What natural satalite orbits Earth?',
-      choice: ['Asteroid', 'The moon', 'Another planet', 'The international space station'],
+      choices: ['A. Asteroid', 'B. The moon', 'C. Another planet', 'D. The international space station'],
       answer: 'The moon'
     },
     {
       question: 'The planet closest to the sun is?',
-      choice: ['Mercury', 'Venus', 'Mars', 'Earth'],
+      choices: ['A. Mercury', 'B. Venus', 'C. Mars', 'D. Earth'],
       answer: 'Mercury'
     },
     {
       question: 'What is the name of the galaxy we live in?',
-      choice: ['Andromeda', 'Milky Way', 'Bode\'s', 'Mayall'],
+      choices: ['A. Andromeda', 'B. Milky Way', 'C. Bode\'s', 'D. Mayall'],
       answer: 'Milky Way'
     },
     {
       question: 'What planet is known for having a ring around it?',
-      choice: ['Mars', 'Jupiter', 'Venus', 'Saturn'],
+      choices: ['A. Mars', 'B. Jupiter', 'C. Venus', 'D. Saturn'],
       answer: 'Saturn'
     },
     {
       question: 'Which two planets have a retrograde(backwards) rotation?',
-      choice: ['Earth and Mars', 'Mars and Venus', 'Jupiter and Saturn', 'Venus and Uranus'],
+      choices: ['A. Earth and Mars', 'B. Mars and Venus', 'C. Jupiter and Saturn', 'D. Venus and Uranus'],
       answer: 'Venus and Uranus'
     }
   ],
   correctFlag: false,
+  currentIdx: 0,
   numberCorrect: 0   
 }
