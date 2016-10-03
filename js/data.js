1ff83dc7e26737380ea849006e3c341cb1c3aeb7
tpecorino <tpecorino@gmail.com>
2016-10-03 13:01:42 -0500

    Updated design to use buttons instead of radio input

--- js/data.js
+++ js/data.js
@@ -3,32 +3,32 @@ var state = {
     {
       question: 'Which planet is NOT a gas giant?',
       choices: ['A. Saturn', 'B. Neptune', 'C. Venus', 'D. Jupiter'],
-      answer: 'Venus'
+      correctIdx: 2
     },
     {
       question: 'What natural satalite orbits Earth?',
       choices: ['A. Asteroid', 'B. The moon', 'C. Another planet', 'D. The international space station'],
-      answer: 'The moon'
+      correctIdx: 1
     },
     {
       question: 'The planet closest to the sun is?',
       choices: ['A. Mercury', 'B. Venus', 'C. Mars', 'D. Earth'],
-      answer: 'Mercury'
+      correctIdx: 0
     },
     {
       question: 'What is the name of the galaxy we live in?',
       choices: ['A. Andromeda', 'B. Milky Way', 'C. Bode\'s', 'D. Mayall'],
-      answer: 'Milky Way'
+      correctIdx: 1
     },
     {
       question: 'What planet is known for having a ring around it?',
       choices: ['A. Mars', 'B. Jupiter', 'C. Venus', 'D. Saturn'],
-      answer: 'Saturn'
+      correctIdx: 3
     },
     {
       question: 'Which two planets have a retrograde(backwards) rotation?',
       choices: ['A. Earth and Mars', 'B. Mars and Venus', 'C. Jupiter and Saturn', 'D. Venus and Uranus'],
-      answer: 'Venus and Uranus'
+      correctIdx: 3
     }
   ],
   correctFlag: false,
